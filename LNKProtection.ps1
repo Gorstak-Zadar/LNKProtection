5f29c423eed5facbc78427f3858fb1bc17903e0d
Goran Štambuk <goran.stambuk@mail2jesus.com>
2026-03-06 15:38:56 +0100

    Update LNKProtection.ps1

--- LNKProtection.ps1
+++ LNKProtection.ps1
@@ -1,5 +1,13 @@
 while($true){
     $s=New-Object -Com WScript.Shell
-    ls "$env:USERPROFILE\Desktop\*.lnk"|?{$s.CreateShortcut($_.FullName).TargetPath -like "\\*"}|rm -Force
+    $paths = "$env:USERPROFILE\Desktop", 
+             "$env:APPDATA\Microsoft\Windows\Start Menu\Programs", 
+             "$env:APPDATA\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar"
+
+    gci $paths -Recurse -Include *.lnk -ErrorAction SilentlyContinue | ? {
+        $s.CreateShortcut($_.FullName).TargetPath -like "\\*"
+    } | rm -Force
+    
     sleep 3600
 }
+
